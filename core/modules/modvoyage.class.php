2bbea5910f84fc3da51f7a9753141dfd64aa508c
atm-orian <orian.capek-messy@atm-consulting.fr>
2022-05-06 10:23:16 +0200

    retour WYSIWYG 2

--- core/modules/modvoyage.class.php
+++ core/modules/modvoyage.class.php
@@ -232,21 +232,21 @@ class modvoyage extends DolibarrModules
 		// $r++;
 
 		$this->rights[$r][0] = $this->numero . $r;	// Permission id (must not be already used)
-		$this->rights[$r][1] = 'voyage';	// Permission label
+		$this->rights[$r][1] = 'read';	// Permission label
 		$this->rights[$r][3] = 0; 					// Permission by default for new user (0/1)
 		$this->rights[$r][4] = 'read';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
 		$this->rights[$r][5] = '';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
 		$r++;
 
 		$this->rights[$r][0] = $this->numero . $r;	// Permission id (must not be already used)
-		$this->rights[$r][1] = 'voyage';	// Permission label
+		$this->rights[$r][1] = 'write';	// Permission label
 		$this->rights[$r][3] = 0; 					// Permission by default for new user (0/1)
 		$this->rights[$r][4] = 'write';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
 		$this->rights[$r][5] = '';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
 		$r++;
 
 		$this->rights[$r][0] = $this->numero . $r;	// Permission id (must not be already used)
-		$this->rights[$r][1] = 'voyage';	// Permission label
+		$this->rights[$r][1] = 'delete';	// Permission label
 		$this->rights[$r][3] = 0; 					// Permission by default for new user (0/1)
 		$this->rights[$r][4] = 'delete';		    // In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
 		$this->rights[$r][5] = '';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
@@ -254,7 +254,7 @@ class modvoyage extends DolibarrModules
 
         $r++;
         $this->rights[$r][0] = $this->numero . $r;
-        $this->rights[$r][1] = 'voyage';
+        $this->rights[$r][1] = 'export';
         $this->rights[$r][3] = 0;
         $this->rights[$r][4] = 'voyage';
         $this->rights[$r][5] = 'export';
